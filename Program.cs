fe76de17e7bb58853e6065f7b2f9e84794239698
DermisZabala <dermiszabala@gmail.com>
2024-08-05 22:35:12 -0400

    Creacion de coleccion LinkedList

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Colecciones_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> misNumeros = new LinkedList<int>();
+
+            int[] Num = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            for (int i = 0; i<Num.Length; i++)
+            {
+                misNumeros.AddFirst(Num[i]);
+            }
+
+            foreach(int elementos in misNumeros)
+            {
+                Console.WriteLine(elementos);
+            }
+        }
+    }
+}
